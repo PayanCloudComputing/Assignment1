9a3960df2fb7e7c61b7bf96cf64e6679d1ab3956
PayanCloudComputing <31172988+PayanCloudComputing@users.noreply.github.com>
2017-09-17 12:47:16 -0700

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 BUCKET=$1
+PROFILE=$2
+
+if [ -z "$PROFILE" ]
+then
+    $PROFILE=default
+fi
+
 l_bucket=$(echo "$BUCKET" | awk '{print tolower($0)}') #Changing bucket name to lower case
 echo "Downloading project..."
 git clone https://github.com/jpayan/CloudComputing.git
@@ -9,17 +16,18 @@ npm install
 echo "Deploying project..."
 grunt build --force
 echo "Creating bucket..."
-if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'
+if aws s3 ls "s3://$S3_BUCKET" --profile $PROFILE 2>&1 | grep -q 'NoSuchBucket'
 then
     echo "A new bucket will be created..."
+    aws s3api create-bucket --bucket $l_bucket --profile $PROFILE
 else
     echo "Bucket already exists."
-    echo "Deleting bucket..."
-    aws s3 rb s3://$BUCKET --force
-    echo "Creating new bucket..."
+    echo "Deleting bucket's content..."
+    aws s3 rm s3://$l_bucket --recursive --profile $PROFILE
 fi
-aws s3api create-bucket --bucket $l_bucket
 echo "Updating files to bucket..."
 cd dist
-aws s3 sync . s3://$l_bucket --acl public-read
-aws s3 website s3://$l_bucket/ --index-document index.html --error-document error.html
+aws s3 sync . s3://$l_bucket --acl public-read --profile $PROFILE
+aws s3 website s3://$l_bucket/ --index-document index.html --error-document 404.html --profile $PROFILE
+cd ../..
+rm -rf CloudComputing
